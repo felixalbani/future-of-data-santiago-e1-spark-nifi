317d1a76c1072fe6430a0ed7482c903ef9e164f9
bag_of_tricks <falbani@hortonworks.com>
2018-06-14 01:19:30 -0400

    script fixes
    script fixes

--- bin/yolov3_setup.sh
+++ bin/yolov3_setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+source set-env.sh
+
 # clone keras-yolo3
 git clone https://github.com/qqwweee/keras-yolo3 ../keras-yolo3
 
 cd ../keras-yolo3
 
-# Download weights
-wget https://pjreddie.com/media/files/yolov3.weights
+if [ ! -f yolov3.weights ]; then
+  # Download weights
+  wget https://pjreddie.com/media/files/yolov3.weights
+fi
 
 # train model
 python convert.py yolov3.cfg yolov3.weights model_data/yolo.h5
