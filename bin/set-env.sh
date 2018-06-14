caaab8f608cd5a39aca0bab9d7daebf75e230152
bag_of_tricks <falbani@hortonworks.com>
2018-06-14 02:20:32 -0400

    set-env.sh

--- bin/set-env.sh
+++ bin/set-env.sh
@@ -3,14 +3,13 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export BASE_DIR="$(dirname $CURRENT_DIR)"
 
-AUX_PATH=/opt/anaconda3/bin
+AUX_PATH=$PATH
+export PATH=/opt/anaconda3/bin:$PATH
+
 VIRTUAL_ENV_PATH=$BASE_DIR/conda_virutal_env
 
 if [ -d $VIRTUAL_ENV_PATH ]; then
   echo "Virtual environment found: $VIRTUAL_ENV_PATH"
   source activate $VIRTUAL_ENV_PATH
-
-  AUX_PATH=$VIRTUAL_ENV_PATH/bin
+  export PATH=$VIRTUAL_ENV_PATH/bin:$AUX_PATH
 fi
-
-export PATH=$AUX_PATH:$PATH
