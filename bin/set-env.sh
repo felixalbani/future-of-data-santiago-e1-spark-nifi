f1fb5b8ceaa4ee9868dcc2091311b21480e94251
falbani <falbani@c24-node2.squadron-labs.com>
2018-06-14 05:44:56 +0000

    Fixed script minor issues

--- bin/set-env.sh
+++ bin/set-env.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-export PATH=/opt/anaconda3/bin:$PATH
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+HOME_DIR="$(dirname $CURRENT_DIR)"
 
-if [ ! -f ../conda_virutal_env ]; then
-  export VIRTUAL_ENV_PATH=../conda_virutal_env
+AUX_PATH=/opt/anaconda3/bin
+
+if [ ! -f $HOME_DIR/conda_virutal_env ]; then
+  export VIRTUAL_ENV_PATH=$HOME_DIR/conda_virutal_env
 
   source activate $VIRTUAL_ENV_PATH
+
+  AUX_PATH=$VIRTUAL_ENV_PATH/bin  
 fi
+
+export PATH=$AUX_PATH:$PATH
