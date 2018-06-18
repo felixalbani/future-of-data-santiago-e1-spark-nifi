9916d5b313390778f2681b021b6da7bc3d62fc92
bag_of_tricks <falbani@hortonworks.com>
2018-06-18 16:12:56 -0400

    added twitter reply and scores to string

--- src/helper.py
+++ src/helper.py
@@ -1,7 +1,12 @@
 import io
+import os
 from PIL import Image
 
 def to_bytearray(image):
     byte_array = io.BytesIO()
     image.save(byte_array, format='PNG')
     return bytearray(byte_array.getvalue())
+
+def create_dir_if_not_exists(dir):
+    if not os.path.exists(dir):
+                os.makedirs(dir)
